88d0462b8944139d729eaa9590b81aaaae515290
carlalaya <carla.laya@gmail.com>
2023-09-18 19:22:01 -0300

    Arreglo Personajes

--- src/Componentes/Paginas/Tarjetas.js
+++ src/Componentes/Paginas/Tarjetas.js
@@ -6,7 +6,7 @@ export default function Tarjetas () {
             <div className='Tarjetas'>
             <p>Aca va la imagen</p>
             <h4>Nombre del Personaje</h4>
-           <button>Personaje</button>
+           <button className='btn'>Personaje</button>
            </div>
         </>
     )
